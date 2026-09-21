7316a66009687838e0920e29dfb0a37169a089f1
davidglenn13 <davidglenn13@hotmail.com>
2026-09-21 16:59:37 -0400

    Use canonical Nassau cash math in Ledger

--- side-bet-cash-beta.js
+++ side-bet-cash-beta.js
@@ -85,7 +85,10 @@
     const net=Object.fromEntries(PLAYERS.map(p=>[p.name,0]));
     for(let r=1;r<=4;r++){
       for(let g=1;g<=2;g++){
-        Object.entries(nassauGroupNet(r,g)).forEach(([n,v])=>net[n]+=Number(v||0));
+        const nassauNet=typeof window.nassauCashNetForGroup==='function'
+          ?window.nassauCashNetForGroup(r,g)
+          :nassauGroupNet(r,g);
+        Object.entries(nassauNet||{}).forEach(([n,v])=>net[n]+=Number(v||0));
       }
       Object.entries(fortyNet(r).net).forEach(([n,v])=>net[n]+=Number(v||0));
     }
